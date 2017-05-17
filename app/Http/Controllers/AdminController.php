20f198605372b80a898fd9c53daea0614eeb2cca
Liutauras Kavaliauskas <liutauraskav@gmail.com>
2017-05-17 12:43:39 +0300

    Updated controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\AdminRequest;
 use App\Models\TravelReports;
 use App\Models\Vehicles;
 use App\User;
+use Carbon\Carbon;
+use DateTime;
 
 
 class AdminController extends Controller
@@ -17,13 +19,16 @@ class AdminController extends Controller
 
     public function showReports(AdminRequest $request, $id)
     {
-        $reports = [];
-        foreach (TravelReports::all() as $report)
-            if($report->created_at->format('m,Y') <= $request->month && $report->getUser->first()->id == $id)
-                $reports[] = $report;
+//        $reports = [];
+//        foreach (TravelReports::all() as $report)
+//            if($report->created_at->format('m,Y') <= $request->month && $report->getUser->first()->id == $id)
+//                $reports[] = $report;
+        //        dd(date('Y-m-d H:i:s'), strtotime($request->month));
 
+        $start =(DateTime::createFromFormat('m/Y', $request->month))->modify('first day of this month');
+        $end = (DateTime::createFromFormat('m/Y', $request->month))->modify('last day of this month');
 
-        return view('transport.user_reports', ['reports' => $reports]);
+        return view('transport.user_reports', ['reports' => TravelReports::where('created_at', '>', $start)->where('created_at', '<', $end)->get()]);
 
     }
 }
